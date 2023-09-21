5ee421301fd32e203b79e03ba0df791b14c256ce
azariaberyl <beryl.naga@gmail.com>
2023-09-21 13:00:39 +0700

    Add unit testing using jest

--- jest/src/index.js
+++ jest/src/index.js
@@ -7,3 +7,18 @@ export function getUser() {
     "username": "John Doe",
   }
 }
+export async function createUser(username, password, addToDatabase) {
+  const id = +new Date();
+  const createdAt = new Date().toISOString();
+  const result = await addToDatabase({
+    id,
+    createdAt,
+    username,
+    password
+  })
+  console.log(result)
+  if (result.status === "success") {
+    return { status: "success" }
+  };
+  throw new Error('Gagal membuat user baru');
+}
--- jest/test/index.test.js
+++ jest/test/index.test.js
@@ -1,4 +1,4 @@
-import { getUser, sum } from '../src'
+import { createUser, getUser, sum } from '../src'
 
 test("sum(1,3) must be 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -9,4 +9,33 @@ test("getUser() must include an id and a username", () => {
     "id": "1",
     "username": "John Doe",
   });
+})
+
+test.todo("create mock test");
+
+test("test mock async return resolved", async () => {
+  const addToDatabase = jest.fn();
+  addToDatabase.mockImplementation((data) => {
+    return new Promise((resolves) => {
+      setTimeout(() => {
+        resolves({
+          status: 'success',
+          data
+        })
+      }, 1000);
+    });
+  })
+
+  await expect(createUser('John', "johndoe", addToDatabase)).resolves.toEqual({
+    status: 'success'
+  });
+  await expect(addToDatabase.mock.results[0].value).resolves.toEqual({
+    status: 'success',
+    data: {
+      id: addToDatabase.mock.calls[0][0].id,
+      createdAt: addToDatabase.mock.calls[0][0].createdAt,
+      username: "John",
+      password: "johndoe"
+    }
+  })
 })
