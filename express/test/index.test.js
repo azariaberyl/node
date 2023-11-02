8fc29a68e16d63aa3ece2f430130d08f87318c02
azariaberyl <beryl.naga@gmail.com>
2023-11-02 18:19:51 +0700

    Add res, req tests

--- express/test/index.test.js
+++ express/test/index.test.js
@@ -1,24 +1,94 @@
-import express from 'express'
+import express from 'express';
 import request from 'supertest';
 
-const app = express()
-app.get('/', (req, res) => {
-  res.send('Halo Dek')
-})
+test('response body', async () => {
+  const app = express();
+  app.get('/', (req, res) => {
+    res.send('Halo Dek');
+  });
 
-app.get('/books', (req, res) => {
-  res.send('No Book')
-})
+  app.get('/books', (req, res) => {
+    res.send('No Book');
+  });
 
-test('Dicoba', async () => {
   let response = await request(app).get('/');
-  expect(response.text).toBe('Halo Dek')
-  
+  expect(response.text).toBe('Halo Dek');
+
   response = await request(app).get('/books');
-  expect(response.text).toBe('No Book')
-})
+  expect(response.text).toBe('No Book');
+});
 
-test('Dcoba 2', async () => {
-  let response = await request(app).get('/');
+test('res status', async () => {
+  const app = express();
+  app.get('/', (req, res) => {
+    if (req.query.name) {
+      res.status(200);
+      res.send('Ok mase');
+    } else {
+      res.status(400);
+    }
+  });
+
+  let response = await request(app).get('/').query({ name: 'John' });
   expect(response.status).toBe(200);
-})
+
+  response = await request(app).get('/');
+  expect(response.status).toBe(400);
+});
+
+test('res set header', async () => {
+  const app = express();
+  app.get('/', (req, res) => {
+    res.set('Token', 'Halo dek');
+  });
+
+  let response = await request(app).get('/').query({ name: 'John' });
+  expect(response.token).toBe('Halo dek');
+});
+
+// Request
+test('req query', async () => {
+  const app = express();
+  app.get('/', (req, res) => {
+    res.send(`${req.query.name}: ${req.query.balance}`);
+  });
+
+  let response = await request(app).get('/').query({
+    name: 'John',
+    balance: 1000,
+  });
+  expect(response.text).toBe('John: 1000');
+});
+
+test('req url', async () => {
+  const app = express();
+  app.get('/path', (req, res) => {
+    res.send(
+      JSON.stringify({
+        originalUrl: req.originalUrl,
+        path: req.path,
+        hostname: req.hostname,
+        protocol: req.protocol,
+      })
+    );
+  });
+
+  let response = await request(app).get('/path').query({
+    name: 'John',
+    balance: 1000,
+  });
+  expect(response.text).toBe(
+    '{"originalUrl":"/path?name=John&balance=1000","path":"/path","hostname":"127.0.0.1","protocol":"http"}'
+  );
+});
+
+test('req get header', async () => {
+  const app = express();
+  app.get('/path', (req, res) => {
+    const type = req.get('Accept');
+    res.send(`The header Accept: ${type}`);
+  });
+
+  let response = await request(app).get('/path').set('accept', 'text/plain');
+  expect(response.text).toBe('The header Accept: text/plain');
+});
