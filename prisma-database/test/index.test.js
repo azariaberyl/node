32a26dab38f997bbaac4da5a541b1baa472ed60f
azariaberyl <beryl.naga@gmail.com>
2023-11-24 15:01:08 +0700

    Add Prisma transactions

--- prisma-database/test/index.test.js
+++ prisma-database/test/index.test.js
@@ -16,16 +16,19 @@ test('Execute SQL', async () => {
 
   const impacted = await prismaClient.$executeRaw`INSERT INTO sample (id, name) VALUES (${id}, ${name})`;
   console.log(impacted);
+  await prismaClient.$executeRaw`DELETE FROM sample WHERE ${id}`;
 });
 
 test('Query SQL', async () => {
+  const name = 'John';
   const id = 1;
 
+  await prismaClient.$executeRaw`INSERT INTO sample (id, name) VALUES (${id}, ${name})`;
   const samples = await prismaClient.$queryRaw`SELECT * FROM sample WHERE id = ${id}`;
   console.log(samples);
 });
 
-describe.only('Prisma CRUD ORM operation', () => {
+describe('Prisma CRUD ORM operation', () => {
   test('Create operation', async () => {
     const customer = await prismaClient.customer.create({
       data: {
@@ -79,5 +82,67 @@ describe.only('Prisma CRUD ORM operation', () => {
     expect(customer.id).toBe('Johnwick');
     expect(customer.name).toBe('John Wick');
     expect(customer.phone).toBe('081234567890');
+  });
+});
+
+test('Tag function', async () => {
+  const name = 'John';
+  const id = 1;
+  function tagFunction(array, ...args) {
+    console.log(array);
+    console.log(args);
+  }
+
+  tagFunction`Hello ${name}, your id is ${id}`;
+});
+
+describe.only('Transaction', () => {
+  test('Sequential transaction', async () => {
+    const insertedData = {
+      email: 'jane@gmail.com',
+      id: 'jane',
+      name: 'Jane',
+      phone: '309213829',
+    };
+    const result = await prismaClient.$transaction([
+      prismaClient.customer.create({
+        data: insertedData,
+      }),
+      prismaClient.customer.delete({
+        where: {
+          id: insertedData.id,
+        },
+      }),
+    ]);
+
+    console.log(result);
+    expect(result[0]).toEqual(insertedData);
+    expect(result[1]).toEqual(insertedData);
+  });
+
+  test('Interactive transaction', async () => {
+    const insertedData = {
+      email: 'jane@gmail.com',
+      id: 'jane',
+      name: 'Jane',
+      phone: '309213829',
+    };
+
+    const result = await prismaClient.$transaction(async (prisma) => {
+      const createJane = await prisma.customer.create({
+        data: insertedData,
+      });
+
+      const delJane = await prisma.customer.delete({
+        where: {
+          id: insertedData.id,
+        },
+      });
+
+      return [createJane, delJane];
+    });
+
+    expect(result[0]).toEqual(insertedData);
+    expect(result[1]).toEqual(insertedData);
   });
 });
